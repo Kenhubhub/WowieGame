a168d413b97dbe17c8c9a8e698cf52a57e520251
el19kjhl <kenluo36@gmail.com>
2022-08-12 12:07:42 +0100

    added event listener to main

--- player.js
+++ player.js
@@ -3,32 +3,7 @@ const v ={
     x: 0,
     y: 0
 }
-window.addEventListener("keydown", e =>{
-    console.log(e.key);
-    switch(e.key){
-        case "ArrowDown": 
-                v.x = 0;
-                v.y = 1;
-                break;
-        case "ArrowUp":
-                v.x = 0;
-                v.y = -1;    
-                break;
-        case "ArrowLeft":
-            v.x = -1;
-            v.y = 0;
-            break;
-        case "ArrowRight":
-            v.x = 1;
-            v.y = 0;    
-            break;
-        default:
-                v.x = 0;
-                v.y = 0
-                break;
 
-    }
-})
 window.addEventListener("keyup", e =>{
     v.x = 0;
     v.y = 0;
